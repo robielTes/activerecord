8ded9078722e244485362246a678caf5ec8b1d28
robielcpnv <robiel.tesfazghi@cpnv.ch>
2022-03-03 09:09:53 +0100

    Fix foreign key for table order_items

--- db/db.sql
+++ db/db.sql
@@ -47,24 +47,6 @@ CREATE TABLE IF NOT EXISTS `poo1_2021_shop`.`products` (
     DEFAULT CHARACTER SET = utf8mb3;
 
 -- -----------------------------------------------------
--- Table `poo1_2021_shop`.`order_items`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `poo1_2021_shop`.`order_items` (
-    `id` INT NOT NULL,
-    `quantity` INT NOT NULL,
-    `item_price` DECIMAL(7,2) NOT NULL,
-    `product_id` INT NOT NULL,
-    PRIMARY KEY (`id`),
-    INDEX `fk_order_items_products1_idx` (`product_id` ASC) VISIBLE,
-    CONSTRAINT `fk_order_items_products1`
-    FOREIGN KEY (`product_id`)
-    REFERENCES `poo1_2021_shop`.`products` (`id`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
-    ENGINE = InnoDB
-    DEFAULT CHARACTER SET = utf8mb3;
-
--- -----------------------------------------------------
 -- Table `poo1_2021_shop`.`orders`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `poo1_2021_shop`.`orders` (
@@ -73,19 +55,39 @@ CREATE TABLE IF NOT EXISTS `poo1_2021_shop`.`orders` (
     `shipped_at` DATETIME NULL DEFAULT NULL,
     `status` VARCHAR(45) NULL DEFAULT NULL,
     `client_id` INT NOT NULL,
-    `order_item_id` INT NOT NULL,
     PRIMARY KEY (`id`),
     INDEX `fk_orders_clients1_idx` (`client_id` ASC) VISIBLE,
-    INDEX `fk_orders_order_items1_idx` (`order_item_id` ASC) VISIBLE,
     CONSTRAINT `fk_orders_clients1`
     FOREIGN KEY (`client_id`)
     REFERENCES `poo1_2021_shop`.`clients` (`id`)
     ON DELETE CASCADE
+    ON UPDATE CASCADE)
+    ENGINE = InnoDB
+    DEFAULT CHARACTER SET = utf8mb3;
+
+-- -----------------------------------------------------
+-- Table `poo1_2021_shop`.`order_items`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `poo1_2021_shop`.`order_items` (
+    `id` INT NOT NULL,
+    `quantity` INT NOT NULL,
+    `item_price` DECIMAL(7,2) NOT NULL,
+    `product_id` INT NOT NULL,
+    `orders_id` INT NOT NULL,
+    PRIMARY KEY (`id`),
+    INDEX `fk_order_items_products1_idx` (`product_id` ASC) VISIBLE,
+    INDEX `fk_order_items_orders1_idx` (`orders_id` ASC) VISIBLE,
+    CONSTRAINT `fk_order_items_products1`
+    FOREIGN KEY (`product_id`)
+    REFERENCES `poo1_2021_shop`.`products` (`id`)
+    ON DELETE CASCADE
     ON UPDATE CASCADE,
-    CONSTRAINT `fk_orders_order_items1`
-    FOREIGN KEY (`order_item_id`)
-    REFERENCES `poo1_2021_shop`.`order_items` (`id`)
+    CONSTRAINT `fk_order_items_orders1`
+    FOREIGN KEY (`orders_id`)
+    REFERENCES `poo1_2021_shop`.`orders` (`id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE)
     ENGINE = InnoDB
     DEFAULT CHARACTER SET = utf8mb3;
+
+
